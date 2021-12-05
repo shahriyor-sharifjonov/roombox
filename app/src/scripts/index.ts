da4f0bdb2a7c14fc0641ba9d5b4bf90fe2436b33
Shahriyor <shahriyordeveloping@gmail.com>
2021-12-05 14:52:23 +0500

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -4,3 +4,9 @@ var swiper = new Swiper(".intro__slider", {
     preloadImages: false,
     lazy: true,
 });
+var swiper = new Swiper(".products__item-slider", {
+    grabCursor: true,
+    loop: true,
+    preloadImages: false,
+    lazy: true,
+});
